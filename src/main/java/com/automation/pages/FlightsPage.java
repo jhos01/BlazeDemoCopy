d1eb559b27c5f14824fa7a2e2ade7fd34b3b1318
jhos01 <jhosluizaga8@gmail.com>
2024-08-21 18:04:15 -0400

    Select the cheapest flight

--- src/main/java/com/automation/pages/FlightsPage.java
+++ src/main/java/com/automation/pages/FlightsPage.java
@@ -6,10 +6,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlightsPage {
     WebDriver driver;
     @FindBy (xpath = "//input[@value='Choose This Flight']")
     private WebElement chooseFlightButton;
+
+    @FindBy(className = "btn btn-small")
+    List<WebElement> buttons;
+
+    @FindBy(tagName = "td")
+    List<WebElement> tdElements;
+
+    List<WebElement> priceList = new ArrayList<>();
 
     public FlightsPage(WebDriver driver){
         this.driver = driver;
@@ -18,5 +29,27 @@ public class FlightsPage {
 
     public void selectFirstFlight(){
         chooseFlightButton.click();
+    }
+
+    public void createPricesList() {
+        for (WebElement td: tdElements){
+            if (td.getText().contains("$")){
+                priceList.add(td);
+            }
+        }
+    }
+    public void selectCheapestFlight(){
+        createPricesList();
+        double smallestNumber = Double.MAX_VALUE;
+        int cheapestFlightPosition = 0;
+        for (int i = 0; i < priceList.size(); i++ ){
+            String priceAsText = priceList.get(i).getText().replaceAll("$", "");
+            double priceAsNumber = Double.parseDouble(priceAsText);
+            if (priceAsNumber < smallestNumber){
+                smallestNumber =priceAsNumber;
+                cheapestFlightPosition = i;
+            }
+        }
+        buttons.get(cheapestFlightPosition).click();
     }
 }
